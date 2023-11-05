8a8cd2dfa2911584a14da15834e5fc003d677431
Gouranshvaishnavji <147132368+Gouranshvaishnavji@users.noreply.github.com>
2023-11-05 19:00:44 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,8 +1,13 @@
 // Iteration 5: Store the player score and display it on the game over screen
-let score = localStorage.getItem("score")
+// Iteration 5: Store the player score and display it on the game over screen
+let score = localStorage.getItem("score");
+console.log(score);
+
 var scoreBoard = document.getElementById("score-board");
-scoreBoard.innerHTML=score;
-var replay = document.getElementById("play-again-button")
-replay.onclick=() => {
-    location.href= "./game.html"
+
+var playAgainButton = document.getElementById("play-again-button");
+
+scoreBoard.innerHTML = score;
+playAgainButton.onclick = (evt) => {
+  location.href = "./game.html";
 };
