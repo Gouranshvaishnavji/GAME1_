376dce0eb4d9d93bcf68c1a891d762339ec2b0fb
Gouranshvaishnavji <147132368+Gouranshvaishnavji@users.noreply.github.com>
2023-11-05 19:05:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// Iteration 1: Make the Play button functional.
-// Description: When the Play button is clicked the game page should be displayed.
-const Play = document.getElementById("play-button");
-Play.onclick= ()  => {
-    location.href = "./game.html";
+const play = document.getElementById("play-button");
+
+play.onclick = () => {
+  location.href = "./game.html";
 };
